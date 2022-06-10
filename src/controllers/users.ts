e644e7e68c65f0e2be0b94cd1e9d7e92c0f10064
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-06-10 15:15:34 +0200

    Added controller to delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -24,7 +24,30 @@ const getUser = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 const updateUser = async (req: Request, res: Response, next: NextFunction) => {}
-const deleteUser = async (req: Request, res: Response, next: NextFunction) => {}
+
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  // get the post id from req.params
+  let id: string = req.params.id
+
+  if(!Types.ObjectId.isValid(id)) {
+    return res.status(404).json({
+      error: 'Post not found'
+    })
+  }
+  // delete the post
+  User.findByIdAndDelete(id, (err: NativeError) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      })
+    }
+
+    return res.status(200).json({
+      message: 'User deleted successfully',
+    })
+  })
+}
+
 const addUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email }: UserProps = req.body
 
